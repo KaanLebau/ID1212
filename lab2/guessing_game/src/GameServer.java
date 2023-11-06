d66b0b9d33555e97f0bb76c20065959c0a3d81b3
KaanLebau <ozsankaan@gmail.com>
2023-11-06 09:28:23 +0100

    guess game model version 1 is done, game controller created

--- lab2/guessing_game/src/GameServer.java
+++ lab2/guessing_game/src/GameServer.java
@@ -5,7 +5,8 @@
 public class GameServer {
 
     public static void main(String[] args) {
-        System.out.println("hello server");
+        System.out.println("Hello there!");
+        System.out.println("General Kenobi");
 
     }
 }
