3a44af3696273d85c73b51e31d615db56ebd2996
Kaan Ozsan <75976200+KaanLebau@users.noreply.github.com>
2023-12-18 11:32:08 +0100

    new structure

--- project/backend/kthForum/src/main/java/dev/kadan/kthForum/ForumRestApi.java
+++ project/backend/kthForum/src/main/java/dev/kadan/kthForum/ForumRestApi.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KthForumApplication {
+public class ForumRestApi {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KthForumApplication.class, args);
+		SpringApplication.run(ForumRestApi.class, args);
 	}
 
 }
