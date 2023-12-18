99a3f185830db9511b6fb543bc809471c5a8bf91
Kaan Ozsan <75976200+KaanLebau@users.noreply.github.com>
2023-12-18 11:11:48 +0100

    kth FORUM then

---
+++ project/kthForum/src/main/java/dev/kadan/kthForum/KthForumApplication.java
@@ -0,0 +1,13 @@
+package dev.kadan.kthForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KthForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KthForumApplication.class, args);
+	}
+
+}
---
+++ project/kthForum/src/test/java/dev/kadan/kthForum/KthForumApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kadan.kthForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KthForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
